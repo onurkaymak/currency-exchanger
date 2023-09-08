dffce5ecdbed4d4d0ed6950b65e8d6a71da1b8c1
onurkaymak <onurkaymak@outlook.com>
2023-09-08 08:57:24 -0700

    Create an Exchanger class and a static method for API calls

---
+++ src/js/exchanger.js
@@ -0,0 +1,16 @@
+export default class Exchanger {
+    static getCurrency(currency = "USD") {
+        return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+            .then(function (response) {
+                if (!response.ok) {
+                    const errorMessage = `${response.status} ${response.statusText}`;
+                    throw new Error(errorMessage);
+                } else {
+                    return response.json();
+                }
+            })
+            .catch(function (error) {
+                return error;
+            });
+    }
+}

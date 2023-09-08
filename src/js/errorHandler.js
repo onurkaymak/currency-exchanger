9eb64a9f96d5ef30db59ae8b9da85828804efa36
onurkaymak <onurkaymak@outlook.com>
2023-09-08 16:20:02 -0700

    Create errorHandler function.

--- src/js/errorHandler.js
+++ src/js/errorHandler.js
@@ -1,7 +1,14 @@
 export const errorHandler = (error) => {
-    console.log(error);
-    document.getElementById('error-title');
-    document.getElementById('error-message');
+    let errorTitle = document.getElementById('error-title');
+    let errorMessage = document.getElementById('error-message');
 
-
+    if (error.status === 404) {
+        // For the "If the query response doesn't include that particular currency" prompt, browser returns 400 for the unknown currencies.//
+        errorTitle.innerText = `Error Code - ${error.status}`;
+        errorMessage.innerText = 'Unsupported currency, please try again with different currency.';
+    } else {
+        // General error handling for other status codes.
+        errorTitle.innerText = `Error Code - ${error.status}`;
+        errorMessage.innerText = `${error.message}`;
+    }
 };

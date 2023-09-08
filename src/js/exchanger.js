14f1a6505691222ec32a03f484074bfbdc13333c
onurkaymak <onurkaymak@outlook.com>
2023-09-08 10:28:27 -0700

    WIP error handling, create defineResults static method for Exchanger class

--- src/js/exchanger.js
+++ src/js/exchanger.js
@@ -1,6 +1,8 @@
 export default class Exchanger {
-    static getCurrency(currency = "USD") {
-        return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+    static currencies = ["usd", 'eur', 'gbp', 'nok', 'sek', 'jpy', 'krw', 'aed', 'rub'];
+
+    static getCurrency(currentCurrency = "usd") {
+        return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currentCurrency}`)
             .then(function (response) {
                 if (!response.ok) {
                     const errorMessage = `${response.status} ${response.statusText}`;
@@ -12,5 +14,20 @@ export default class Exchanger {
             .catch(function (error) {
                 return error;
             });
+    }
+
+    static defineResults(apiResults) {
+        console.log(apiResults);
+        // if (Exchanger.currencies.includes(currentCurrency) && Exchanger.currencies.includes(convertToCurrency)) {
+        //     console.log("yes");
+        // } else {
+        //     return console.log("no");
+        // }
+
+        if (this.currencies.includes(apiResults.base_code.toLowerCase())) {
+            console.log('yes');
+        } else {
+            const errorMessage = "We don't have this currencie"
+        }
     }
 }

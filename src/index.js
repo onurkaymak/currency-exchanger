a058f6f10f795e9d4736aebd16caeef5ec6e7091
onurkaymak <onurkaymak@outlook.com>
2023-09-08 10:54:26 -0700

    Create formHandler with try&catch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
+import Exchanger from './js/exchanger';
 import './css/styles.css';
 
+const formHandler = async (event) => {
+    event.preventDefault();
+    let currentCurrency = event.target[0].value;
+    let convertToCurrency = event.target[1].value;
 
+
+    try {
+        const apiResult = await Exchanger.getCurrency(currentCurrency, convertToCurrency);
+        Exchanger.defineResults(apiResult, currentCurrency, convertToCurrency);
+    } catch (error) {
+        console.log(error);
+    }
+
+};
+
+
+window.addEventListener("load", function () {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", formHandler);
+});

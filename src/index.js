0611134dacec5b3a4e11c4346e60ead4c04e4a2c
onurkaymak <onurkaymak@outlook.com>
2023-09-08 11:51:50 -0700

    WIP error handling

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import Exchanger from './js/exchanger';
 import './css/styles.css';
 
+
+export const printResults = (results) => {
+    console.log(results); //// REFLECT THE RESULTS
+};
+
 const formHandler = async (event) => {
     event.preventDefault();
     let currentCurrency = event.target[0].value;
     let convertToCurrency = event.target[1].value;
 
+    // try {
+    //     const apiResult = await Exchanger.getCurrency(currentCurrency, convertToCurrency);
+    //     Exchanger.defineResults(apiResult, currentCurrency, convertToCurrency);
+    // } catch (error) {
+    //     console.log(error, 'index.js catch');
+    // }
 
-    try {
-        const apiResult = await Exchanger.getCurrency(currentCurrency, convertToCurrency);
-        Exchanger.defineResults(apiResult, currentCurrency, convertToCurrency);
-    } catch (error) {
-        console.log(error);
+
+
+    const response = await Exchanger.getCurrency(currentCurrency, convertToCurrency);
+    if (response.main) {
+        console.log('print results');
+    } else {
+        console.log('print errors');
     }
 
 };
